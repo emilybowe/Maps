d452ce001067f714a706837fb56ad5d795843848
Emily Bowe <ebowe@tcd.ie>
2020-03-29 15:28:03 +0100

    Added Product class with constructors, accessors, fields

---
+++ src/com/Maps/Product.java
@@ -0,0 +1,16 @@
+package com.Maps;
+
+public class Product {
+    private int id;
+    private String name;
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    Product() {
+
+    }
+}
