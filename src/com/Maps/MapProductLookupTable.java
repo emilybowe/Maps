1cdbcfb25aa65e1ddc84adf946d245dd89c14104
Emily Bowe <ebowe@tcd.ie>
2020-03-29 14:41:55 +0100

    Added Map - MapProductLookupTable which implements interface

---
+++ src/com/Maps/MapProductLookupTable.java
@@ -0,0 +1,18 @@
+package com.Maps;
+
+public class MapProductLookupTable implements ProductLookupTable {
+    @Override
+    public void addProduct(int Id, String Product) {
+
+    }
+
+    @Override
+    public String lookUpProductById(int Id) {
+        return null;
+    }
+
+    @Override
+    public void removeProduct(int Id) {
+
+    }
+}
