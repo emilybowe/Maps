0a737ec9fdb3f7b379ee91575678dc421d357020
Emily Bowe <ebowe@tcd.ie>
2020-03-29 15:30:24 +0100

    Added HashMap and Interface Implementation

--- src/com/Maps/MapProductLookupTable.java
+++ src/com/Maps/MapProductLookupTable.java
@@ -1,18 +1,22 @@
 package com.Maps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapProductLookupTable implements ProductLookupTable {
+    private Map<Integer, Product> idToProduct = new HashMap<>();
     @Override
-    public void addProduct(int Id, String Product) {
-
+    public void addProduct(Product productToAdd) {
+        if(idToProduct.containsKey(productToAdd.getId())) {
+            throw new IllegalArgumentException();
+        }
+        idToProduct.put(productToAdd.getId(), productToAdd);
     }
-
     @Override
-    public String lookUpProductById(int Id) {
-        return null;
+    public Product lookupById(int id) {
+        return idToProduct.get(id);
     }
-
     @Override
-    public void removeProduct(int Id) {
-
+    public void clear() {
     }
 }
